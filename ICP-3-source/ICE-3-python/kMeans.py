1933bbf4ff0310b1767af080e1e67ea18424402e
Wayne Nguyen <wayneie_n@hotmail.com>
2018-02-02 17:04:34 -0600

    icp 3 source

--- ICP-3-source/ICE-3-python/kMeans.py
+++ ICP-3-source/ICE-3-python/kMeans.py
@@ -10,14 +10,17 @@ if __name__ == "__main__":
     conf = SparkConf().setMaster("local")
     sc = SparkContext(conf=conf)
 
-    spatial_data = sc.textFile("kmeans_data.txt")
-    parsed_data = spatial_data.map(lambda x: array([float(y) for y in x.split(' ')]))
+    # Load and parse data
+    spatial_data = sc.textFile("3D_spatial_network.txt")
+    parsed_data = spatial_data.map(lambda x: array([float(y) for y in x.split(',')]))
 
     print(parsed_data.collect())
 
+    # Make data into 3 and 4 clusters
     clusters = KMeans.train(parsed_data, 3, maxIterations=10, initializationMode="random")
     clusters1 = KMeans.train(parsed_data, 4, maxIterations=10, initializationMode="random")
 
+    # function for error
     def error(point):
         center = clusters.centers[clusters.predict(point)]
         return sqrt(sum([x ** 2 for x in (point - center)]))
@@ -26,13 +29,13 @@ if __name__ == "__main__":
         center = clusters1.centers[clusters1.predict(point)]
         return sqrt(sum([x ** 2 for x in (point - center)]))
 
+    # Formula to get Within Set Sum of Squared Error
     wssse = parsed_data.map(lambda p: error(p)).reduce(lambda x, y: x+y)
     print("WSSSE = " + str(wssse))
-
     wssse1 = parsed_data.map(lambda p: error1(p)).reduce(lambda x, y: x+y)
     print("WSSSE1 = " + str(wssse1))
 
-    # clusters.predict(parsed_data).zip(parsed_data).foreach(f=>print(f[0],f[1]))
-    # clusters.save(sc, "3cluster")
+    # save to file
+    clusters.save(sc, "3cluster")
 
     sc.stop()
--- ICP-3-source/ICE-3-python/linearR.py
+++ ICP-3-source/ICE-3-python/linearR.py
@@ -14,24 +14,26 @@ if __name__ == "__main__":
         values = [float(x) for x in line.replace(',', ' ').split(' ')]
         return LabeledPoint(values[0], values[1:])
 
+    # Make RDD and parse data
     spatial_data = sc.textFile("lpsa.data")
     parsed_data = spatial_data.map(parse_point)
 
     print(parsed_data.collect())
 
+    # Split data in 70% training and 30% test
     (train_data, test_data) = parsed_data.randomSplit([0.7, 0.3])
 
+    # make linear regression model
     lr_model = LinearRegressionWithSGD.train(train_data, iterations=50, step=.000001)
 
+    # use the predict function and then calculate the MSE for the training data
     vp = train_data.map(lambda x: (x.label, lr_model.predict(x.features)))
-
-    print(vp.collect())
-
     mse = vp.map(lambda r: (r[0] - r[1])**2).reduce(lambda x, y: x + y) / vp.count()
+    print(vp.collect())
     print("Training MSE = " + str(mse))
 
+    # use the predict function and then calculate the MSE for the test data
     vp1 = test_data.map(lambda x: (x.label, lr_model.predict(x.features)))
-
     mse1 = vp1.map(lambda r: (r[0] - r[1]) ** 2).reduce(lambda x, y: x + y) / vp1.count()
     print("Test MSE = " + str(mse1))
 
